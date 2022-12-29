4b199c4fa05f6a2e557f5a83440046038f948c5e
alyssa <alyssa@uni-koblenz.de>
2022-12-29 19:50:06 +0100

    ModelLoader but worse

--- Source/Engine/ModelLoader.cpp
+++ Source/Engine/ModelLoader.cpp
@@ -24,6 +24,48 @@ bool ModelLoader::loadModel(std::string path)
         return false;
     }
 
+    // Iterate over all meshes in the model
+    for (unsigned int i = 0; i < scene->mNumMeshes; i++)
+    {
+        aiMesh *mesh = scene->mMeshes[i];
+
+        // Indices
+        int index = mesh->mNumFaces*3;
+        m_index.push_back(index);
+        
+        // Extract the vertices, normals, and textures from the mesh
+        for (unsigned int j = 0; j < mesh->mNumVertices; j++)
+        {
+            // Vertices
+            aiVector3D vertex = mesh->mVertices[j];
+            glm::vec4 vertexElem = glm::vec4(vertex.x, vertex.y, vertex.z, 1.0f);
+            m_vertices.push_back(vertexElem);
+            
+            // Normals
+            aiVector3D normal = mesh->mNormals[j];
+            glm::vec3 normalElem = glm::vec3(normal.x, normal.y, normal.z);
+            m_normals.push_back(normalElem);
+            
+            // Textures
+            if (mesh->mTextureCoords[0])
+            {
+                aiVector3D texCoord = mesh->mTextureCoords[0][j];
+                glm::vec2 texElem = glm::vec2(texCoord.x, texCoord.y);
+                m_uvs.push_back(texElem);
+            }
+            else
+            {
+                // If the mesh does not have texture coordinates, use a default value of (0,0)
+                m_uvs.push_back(glm::vec2(0.0f, 0.0f));
+            }
+
+            // Tangents (what if they don't exist?)
+            aiVector3D tangent = mesh->mTangents[j];
+            glm::vec3 tangentElem = glm::vec3(tangent.x, tangent.y, tangent.z);
+            m_tangents.push_back(tangentElem);
+            // If the mesh contains tangents, it automatically also contains bitangents.
+        }
+    }
     return true;
 }
 
