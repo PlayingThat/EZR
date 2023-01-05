13ace168323efa521926518c9738020df858453a
mbreibach <breibach8@gmail.com>
2023-01-05 14:43:28 +0100

    Add textured shader to ghost

--- Source/Engine/Texture.cpp
+++ Source/Engine/Texture.cpp
@@ -188,7 +188,10 @@ GLuint createTextureFromFile(std::string path)
                 break;
         }
 
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        if (glformat == GL_RGBA8)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        else
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
 
     } else
--- Source/Objects/Ghost.cpp
+++ Source/Objects/Ghost.cpp
@@ -13,8 +13,8 @@ Ghost::Ghost(std::shared_ptr<Scene> scene) : Drawable(scene)
     create();
 
     // Shader setup
-    m_basicVertexShader = std::make_shared<Shader>("./Assets/Shader/Gooch.vert");
-    m_textureFragmentShader = std::make_shared<Shader>("./Assets/Shader/Gooch.frag");
+    m_basicVertexShader = std::make_shared<Shader>("./Assets/Shader/Basic.vert");
+    m_textureFragmentShader = std::make_shared<Shader>("./Assets/Shader/BasicTextured.frag");
 
     m_basicShaderProgram = std::make_unique<ShaderProgram>("Gooch");
     m_basicShaderProgram->addShader(m_basicVertexShader);
@@ -30,15 +30,15 @@ void Ghost::create()
     m_model->loadModel("Assets/Models/Ghost.fbx", m_vertices, m_normals, m_uvs, m_indices, m_tangents);
     createBuffers();
 
-    // Load texture
-    
+    Drawable::setBaseRotation(glm::vec3(1.0f, 0.0f, 0.0f), 90.0f);
 
     // Silence is golden
 }
 
 void Ghost::draw()
 {
-    Drawable::setRotation(glm::vec3(1.0f, 0.0f, 0.0f), 90.0f);
+    Drawable::rotate(glm::vec3(0.0f, 0.0f, 1.0f), 90.0f);
+
     m_basicShaderProgram->use();
 
     m_basicShaderProgram->setMat4("projectionMatrix", *m_scene->getState()->getCamera()->getProjectionMatrix());
