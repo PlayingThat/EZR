82a980f92d8e8772b936c0eecab84606e9dd8cc2
PlayingThat <webmaster@playingthat.de>
2023-01-26 23:49:54 +0100

    Fix binding of pbr textures

--- Source/Engine/Mesh.cpp
+++ Source/Engine/Mesh.cpp
@@ -40,26 +40,32 @@ Mesh::Mesh(std::vector<glm::vec4> vertices,
 
 void Mesh::draw()
 {
-    // Bind the shader program
-    //m_shaderProgram->use();
+    // Get active shader program and set the diffuse color
+    GLint prog = 0;
+    glGetIntegerv(GL_CURRENT_PROGRAM, &prog);
+    glUseProgram(prog);
 
     // Bind the textures
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, m_diffuseTexture);
+    glUniform1i(glGetUniformLocation(prog, "diffuseSampler"), 0);
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, m_metallicTexture);
+    glUniform1i(glGetUniformLocation(prog, "metalSampler"), 1);
     glActiveTexture(GL_TEXTURE2);
     glBindTexture(GL_TEXTURE_2D, m_smoothnessTexture);
+    glUniform1i(glGetUniformLocation(prog, "smoothnessSampler"), 2);
     glActiveTexture(GL_TEXTURE3);
     glBindTexture(GL_TEXTURE_2D, m_heightTexture);
+    glUniform1i(glGetUniformLocation(prog, "heightSampler"), 3);
     glActiveTexture(GL_TEXTURE4);
     glBindTexture(GL_TEXTURE_2D, m_normalTexture);
+    glUniform1i(glGetUniformLocation(prog, "normalSampler"), 4);
     glActiveTexture(GL_TEXTURE5);
     glBindTexture(GL_TEXTURE_2D, m_ambientOcculsionTexture);
+    glUniform1i(glGetUniformLocation(prog, "ambientOcclusionSampler"), 5);
 
-    // Get active shader program and set the diffuse color
-    GLint prog = 0;
-    glGetIntegerv(GL_CURRENT_PROGRAM, &prog);
+
     GLuint uniformLocation = glGetUniformLocation(prog, "DiffuseColor");
     glUniform4fv(uniformLocation, 1, glm::value_ptr(m_diffuseColor));
     
