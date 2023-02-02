b190f0563f4e8cca9d0ee9e35c21fc94b4237e7c
mbreibach <breibach8@gmail.com>
2023-02-02 18:34:25 +0100

    Clean up

--- Source/Objects/Ghost.cpp
+++ Source/Objects/Ghost.cpp
@@ -11,18 +11,6 @@ Ghost::Ghost(std::shared_ptr<Scene> scene) : Drawable(scene)
 {
     m_model = std::make_unique<Model>(m_scene);
     create();
-
-    /* Deprecated */
-    // // Shader setup
-    // m_basicVertexShader = std::make_shared<Shader>("./Assets/Shader/Basic.vert");
-    // m_textureFragmentShader = std::make_shared<Shader>("./Assets/Shader/BasicTextured.frag");
-
-    // m_basicShaderProgram = std::make_unique<ShaderProgram>("Gooch");
-    // m_basicShaderProgram->addShader(m_basicVertexShader);
-    // m_basicShaderProgram->addShader(m_textureFragmentShader);
-    // printf("Linking shader program...\n");
-
-    // m_basicShaderProgram->link();
 }
 
 void Ghost::create()
@@ -39,16 +27,6 @@ void Ghost::create()
 void Ghost::draw()
 {
     Drawable::rotate(glm::vec3(0.0f, 0.0f, 1.0f), 90.0f);
-
-    // m_basicShaderProgram->use();
-
-    // m_basicShaderProgram->setMat4("projectionMatrix", *m_scene->getState()->getCamera()->getProjectionMatrix());
-    // m_basicShaderProgram->setMat4("viewMatrix", *m_scene->getState()->getCamera()->getViewMatrix());
-    // m_basicShaderProgram->setMat3("normalMatrix", glm::mat3(
-    //                                                 glm::inverseTranspose(*m_scene->getState()->getCamera()->getViewMatrix() * 
-    //                                                 Drawable::getModelMatrix())));
-    // m_basicShaderProgram->setMat4("modelMatrix", Drawable::getModelMatrix());
-    // m_basicShaderProgram->setVec3("vColor", glm::vec3(0.3f, 0.4f, 0.0f));
 
     m_model->draw();
 }
